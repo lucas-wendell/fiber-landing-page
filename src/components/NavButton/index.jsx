b5ab9ce6bc653cfff5da2f4b572e5209346ae8a1
Lucas Wendell <lucaswhendellf@gmail.com>
2023-02-02 15:54:10 -0300

    style: styling button nav

--- src/components/NavButton/index.jsx
+++ src/components/NavButton/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './style.css';
 
 export const NavButton = ({ state, children }) => {
+	const styles =
+		state === 'primary'
+			? { color: '#fff', backgroundColor: '#4e13d1' }
+			: { color: '#0c0b07', backgroundColor: 'transparent' };
+
 	return (
-		<button style={{ background: state === 'primary' ? 'red' : 'blue' }}>
+		<button style={{ ...styles }} className="button">
 			{children}
 		</button>
 	);
--- src/components/NavButtonContainer/index.jsx
+++ src/components/NavButtonContainer/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavButton } from '../NavButton';
+import './style.css';
 
 export const NavButtonContainer = () => {
 	return (
 		<div className="navButtonContainer">
-			<NavButton state={'primary'}>Sign In</NavButton>
-			<NavButton state={'secundary'}>Sign Up</NavButton>
+			<NavButton state={'secondary'}>Sign In</NavButton>
+			<NavButton state={'primary'}>Sign Up</NavButton>
 		</div>
 	);
 };
