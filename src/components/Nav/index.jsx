b0f616f0f6cead2881895af7677f536031b81c39
Lucas Wendell <lucaswhendellf@gmail.com>
2023-02-02 15:35:06 -0300

    style: styling nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavButtonContainer } from '../NavButtonContainer';
-
+import './style.css';
 export const Nav = () => {
 	return (
 		<nav className="nav">
-			<h6 className="log">Fiber</h6>
+			<h6 className="logo">Fiber</h6>
 			<ul className="links">
 				<li>Community</li>
 				<li>Pricing</li>
