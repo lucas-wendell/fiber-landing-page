4cfbf61ef578b8264e08afc8673b78079e26bf51
Lucas Wendell <lucaswhendellf@gmail.com>
2023-03-11 10:44:26 -0300

    teste

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,15 @@
 import './style.css';
-import { useContext, useEffect, useState } from 'react';
 
-import { DescriptionSection } from '../../components/DescriptionSection';
 import { Footer } from '../../components/Footer';
-
+import { DescriptionSection } from '../../components/DescriptionSection';
 import { InformationSection } from '../../components/InformationSection';
-import { GlobalContext, Provider } from '../../context';
 
 export const Home = () => {
-	const [contextState, setContextState] = useState();
-	/* const globalContext = useContext(GlobalContext);
-
-	const handleSetUser = () => {
-		// globalContext.forEach(user => {
-		// 	localStorage.setItem(user.email, JSON.stringify(user));
-		// });
-		setContextState({state})
-	};
-
-	useEffect(() => {
-		handleSetUser();
-	}, []);
-
-	useEffect(() => {
-		handleSetUser();
-	}, [contextState]); */
-
 	return (
 		<>
-			{/* <GlobalContext.Provider value={{ contextState, setContextState }}> */}
-			<DescriptionSection />;
+			<DescriptionSection />
 			<InformationSection />
 			<Footer />
-			{/* </GlobalContext.Provider> */}
 		</>
 	);
 };
